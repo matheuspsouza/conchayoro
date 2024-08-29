5984f83fdacf522f24af93c2ce03dd85bb9f3449
Matheus Patrocínio Souza <36196108+matheuspsouza@users.noreply.github.com>
2024-08-29 19:12:06 -0300

    Update page.tsx

--- my-app-frontend/src/app/page.tsx
+++ my-app-frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-monotext-sm lg:flex">
-        <h1> Welcome to the ConchaYOro App </h1>
+        <h1> Welcome to the ConchaYOro App - Matheus Patrocínio Souza </h1>
       </div>
     </main>
   )
