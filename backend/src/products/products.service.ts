13caf4a375393ccb8ff22afdfd1450f1947a270b
matheuspsouza <matheuspatrocinio@hotmail.com>
2024-09-06 20:37:51 -0300

    feat: Integration tests

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -51,4 +51,10 @@ export class ProductsService {
     await product.update(productUpdated);
     return product;
   }
+
+  async findByCriteria(criteria: any): Promise<Product[]> {
+    return this.productModel.findAll({
+      where: criteria,
+    });
+  }
 }
