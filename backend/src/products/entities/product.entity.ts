c24fcabc765c90e65b9f7d2b148a127628e3ef58
matheuspsouza <matheuspatrocinio@hotmail.com>
2024-09-05 21:13:28 -0300

    feat: Adding Sequelize and updating products Crud

--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -1,1 +1,24 @@
-export class Product {}
+import { UUIDV4 } from 'sequelize';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+@Table({ tableName: 'Product', createdAt: true, updatedAt: true })
+export class Product extends Model {
+  @Column({
+    type: DataType.UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+    allowNull: false,
+  })
+  id: string;
+
+  @Column({ allowNull: false, unique: true })
+  name: string;
+
+  @Column({})
+  price: number;
+
+  @Column({})
+  category: string;
+
+  @Column({})
+  rating: number;
+}
