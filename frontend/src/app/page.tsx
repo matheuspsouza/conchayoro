df5778bcc7680ef50a05cbef31c1214472e9fb3c
matheuspsouza <matheuspatrocinio@hotmail.com>
2024-09-12 21:12:09 -0300

    feat: feature flag true

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
-export default function Home() {
+import { blackFridayFlag } from '@/app/flag';
+
+export default async function Page() {
+  const black_friday = await blackFridayFlag();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-monotext-sm lg:flex">
-        <h1> Welcome to the ConchaYOro App - Matheus Patrocínio Souza - V2.1 </h1>
-      </div>
-    </main>
+    <div>
+      <h1> Welcome to the ConchaYOro App - deploy v3 Matheus Patrocínio Souza</h1>
+        { black_friday ? <button> Promo </button> : <button> Normal </button> }
+    </div>
   )
-}
+};
